183f80b0cc84df70773755068a312b1be2072168
BenjaminAJ <ajanakub30@gmail.com>
2023-04-17 07:18:37 +0100

    created deleteItem functions

--- js/phonebookmodule.js
+++ js/phonebookmodule.js
@@ -1,6 +1,7 @@
 const keys = document.querySelectorAll('.keys');
 const keypadInput = document.querySelector('#keypadInput');
 const dialBtn = document.getElementById('dial');
+const deleteBtn = document.getElementById('deleteBtn');
 
 
 for (let index = 0; index < keys.length; index++) {
@@ -12,7 +13,15 @@ for (let index = 0; index < keys.length; index++) {
 }
 
 dialBtn.addEventListener('click', dial);
+deleteBtn.addEventListener('click', deleteItem);
 
 function dial(params) {
     console.log('I am dialing...');
-}   
+}  
+function deleteItem(params) {
+    let previousContent = keypadInput.value;
+    let newContent = previousContent.slice(0,-1);
+
+    keypadInput.value = newContent;
+} 
+
